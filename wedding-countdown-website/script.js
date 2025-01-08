ead298677d835cbf89425e8443964a6ff64b071d
jumayevd <don.jumayev@newuu.uz>
2025-01-08 12:24:10 +0500

    F

--- wedding-countdown-website/script.js
+++ wedding-countdown-website/script.js
@@ -1,1 +1,33 @@
+// Particle effect
+const particlesContainer = document.querySelector('.particles');
+
+function createParticle() {
+  const particle = document.createElement('div');
+  particle.classList.add('particle');
+  
+  // Random size for particles
+  const size = Math.random() * 3 + 1;
+  particle.style.width = `${size}px`;
+  particle.style.height = `${size}px`;
+  
+  // Positioning particles at random locations
+  const xPos = Math.random() * 100;
+  const yPos = Math.random() * 100;
+  particle.style.left = `${xPos}vw`;
+  particle.style.top = `${yPos}vh`;
+  
+  // Random animation timing for variation
+  const duration = Math.random() * 5 + 4; // 4 to 9 seconds
+  particle.style.animationDuration = `${duration}s`;
+  
+  particlesContainer.appendChild(particle);
+
+  // Remove particle after animation
+  setTimeout(() => {
+    particle.remove();
+  }, duration * 1000);
+}
+
+// Generate particles every 150ms
+setInterval(createParticle, 150);
 
